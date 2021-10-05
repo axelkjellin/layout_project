c5653770d37b0ad99b4967101ce398dffce988ad
Axel Kjellin <axel@bussolasocial.com.br>
2021-10-05 06:41:44 -0300

    config do ambiente

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <div class="nav">
+      <router-link to="/">Home</router-link>|<router-link to="/about"
+        >About</router-link
+      >
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap");
+@import 'src/assets/app.scss';
+
+</style>
